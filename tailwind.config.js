ed6b9bcbc27b5bbbe9eafd19986c73d904ab59eb
williamlunelli <williamlunelli07@gmail.com>
2024-10-15 11:36:17 -0300

    Tela de login com o tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'vermelho' : "#FF0000",
+        'vermelho-escuro': "#990000"
+      }
+    },
+  },
+  plugins: [],
+}
+
+
